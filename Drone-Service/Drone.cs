e4b7c111f1819f0ee7b22d4925810f98972c0976
Milo Kester <kester046@gmail.com>
2024-04-29 11:07:01 +0800

    Add project files.

---
+++ Drone-Service/Drone.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drone_Service
+{
+    // 6.1	Create a separate class file to hold the data items of the Drone.
+    // Use separate getter and setter methods, ensure the attributes are private and the accessor methods are public.
+    // Add a display method that returns a string for Client Name and Service Cost.
+    // Add suitable code to the Client Name and Service Problem accessor methods so the data is formatted as Title case or Sentence case.
+    // Save the class as “Drone.cs”.
+    internal class Drone
+    {
+        // Attributes
+        private string client;
+        private string model;
+        private string problem;
+        private int cost;
+        private int tag;
+
+        // getter
+        public string GetClient()
+        {
+            return client;
+        }
+        
+        // setter
+        public void SetClient(string newClient)
+        {
+            client = newClient;
+        }
+
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public void SetModel(string newModel)
+        {
+            model = newModel;
+        }
+
+        public string GetProblem()
+        {
+            return problem;
+        }
+
+        public void SetProblem(string newProblem)
+        {
+            problem = newProblem;
+        }
+
+        public int GetCost()
+        {
+            return cost;
+        }
+
+        public void SetCost(int newCost)
+        {
+            cost = newCost;
+        }
+
+        public int GetTag()
+        {
+            return tag;
+        }
+
+        public void SetTag(int newTag)
+        {
+            tag = newTag;
+        }
+
+    }
+}
---
+++ Drone-Service/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drone_Service
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // 6.2	Create a global List<T> of type Drone called “FinishedList”.
+        List<Drone> FinishedList = new List<Drone>();
+        // 6.3	Create a global Queue<T> of type Drone called “RegularService”.
+        Queue<Drone> RegularService = new Queue<Drone>();
+        // 6.4	Create a global Queue<T> of type Drone called “ExpressService”.
+        Queue<Drone> ExpressService = new Queue<Drone>();
+        
+    }
+}
